479c72064195d4c96c94996968f89496e9d5e4e3
Prateek Prasad <leavethemails@gmail.com>
2023-12-18 12:11:34 +0530

    add basic content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
-    return (
-        <div className="overflow-hidden mx-11"> rounded-lg bg-black
-            <div className="px-4 py-5 sm:p-6">
-                <p className="text-xl">Hello </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="mx-auto rounded-md bg-white p-8">
+      <div>
+        <p>Available for projects</p>
+      </div>
+      <div className="mt-16 space-y-4">
+        <p>
+          👋🏻 I am <span className="font-medium">Prateek</span>, a product
+          engineer from Bangalore, India. I specialize in creating native iOS &
+          Android apps. I have a strong eye for design & UX and have published
+          several books on mobile design & development.
+        </p>
+        <p>
+          In my free time, I like to mentor beginners in tech, break in to the
+          industry
+        </p>
+      </div>
+      <div className="mt-8 font-light">
+        <p>Skills - Swift, Kotlin, Kotlin Multiplatform, React, TypeScript</p>
+      </div>
+    </div>
+  );
 }
