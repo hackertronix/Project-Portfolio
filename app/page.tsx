a7cca4502ace37eb9f8167fb027a81035662791b
Prateek Prasad <leavethemails@gmail.com>
2024-01-09 17:19:13 +0530

    add updated page

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,88 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="mx-auto rounded-md bg-white p-8">
-      <div>
-        <p>Available for projects</p>
-      </div>
-      <div className="mt-16 space-y-4">
-        <p>
-          👋🏻 I am <span className="font-medium">Prateek</span>, a product
-          engineer from Bangalore, India. I specialize in creating native iOS &
-          Android apps. I have a strong eye for design & UX and have published
-          several books on mobile design & development.
-        </p>
-        <p>
-          In my free time, I like to mentor beginners in tech, break in to the
-          industry
-        </p>
-      </div>
-      <div className="mt-8 font-light">
-        <p>Skills - Swift, Kotlin, Kotlin Multiplatform, React, TypeScript</p>
+    // Surface
+    <div className="relative min-h-screen bg-custom-color">
+      {/* Radial gradient */}
+      <div className="bg-halo-gradient fixed flex h-screen w-screen items-center justify-center">
+        {/* Card */}
+        <div className="relative mx-12 max-w-sm rounded-xl bg-white shadow-xl sm:mx-auto sm:max-w-sm md:mx-auto">
+          <div className="mx-auto p-6">
+            {/* Pill */}
+            <div className="flex max-w-fit items-center justify-evenly rounded-full bg-salmon-200 p-1 shadow-inner">
+              {/* Ping */}
+              <div className="relative ml-1 flex h-3 w-3 items-center justify-center">
+                <div className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white opacity-75" />
+                <div className="h-2 w-2 rounded-full bg-white" />
+              </div>
+
+              <div className="text px-2 text-xs font-light text-salmon-600">
+                <p>Available for projects</p>
+              </div>
+
+              <Image
+                src="/arrow.svg"
+                width={22}
+                height={22}
+                alt="Contact Prateek"
+              />
+            </div>
+            <div className=" text-sm font-light tracking-normal text-salmon-900">
+              <div className="mt-8 space-y-4 ">
+                <p>
+                  👋🏻 I am <span className="font-medium">Prateek</span>, a
+                  product engineer from Bangalore, India. I specialize in
+                  creating native iOS & Android apps. I have a strong eye for
+                  design & UX and have published several books on mobile design
+                  & development.
+                </p>
+                <p>
+                  In my free time, I like to mentor beginners in tech, break in
+                  to the industry
+                </p>
+              </div>
+              <div className="mt-8 font-light">
+                <p>
+                  Skills - Swift, Kotlin, Kotlin Multiplatform, React,
+                  TypeScript
+                </p>
+              </div>
+
+              <ul className="mt-16 space-y-6 text-sm font-light tracking-normal text-salmon-900">
+                <li className="flex items-center">
+                  <Image
+                    src="/linkedin.svg"
+                    width={24}
+                    height={24}
+                    alt="Picture of the author"
+                  />
+
+                  <p className="px-6">Work experience & timeline</p>
+                </li>
+                <li className="flex items-center">
+                  <Image
+                    src="/twitter.svg"
+                    width={24}
+                    height={24}
+                    alt="Picture of the author"
+                  />
+
+                  <p className="px-6">Shoot me a DM</p>
+                </li>
+                <li className="flex items-center">
+                  <Image
+                    src="/github.svg"
+                    width={24}
+                    height={24}
+                    alt="Picture of the author"
+                  />
+
+                  <p className="px-6">Stuff I work on for fun</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,9 +7,33 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'custom-color': '#EFF2F6',
+      },
+
+      backgroundImage: {
+        'halo-gradient': 'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(252,245,234,1) 34%, rgba(252,245,234,1) 50%, rgba(255,255,255,1) 100%)'
+      },
+      
+      colors: {
+        salmon: {
+          '50': '#fef8ee',
+          '100': '#fdf0d7',
+          '200': '#f9d9a7',
+          '300': '#f6c37b',
+          '400': '#f1a046',
+          '500': '#ed8422',
+          '600': '#de6b18',
+          '700': '#b85116',
+          '800': '#934119',
+          '900': '#8C7147',
+          '950': '#401a0a',
+        },
+      },
+    },
   },
   emmetCompletions: true,
   plugins: [],
-}
-export default config
+};
+export default config;
